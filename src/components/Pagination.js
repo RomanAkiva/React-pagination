7a6cff61b81decd62da9a3761317da355a3ae603
RomanOriginal <k7ngusha@gmail.com>
2019-07-03 19:26:00 +0300

    init commit

---
+++ src/components/Pagination.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Pagination = ({ postsPerPage, totalPosts, paginate}) => {
+    const pageNumbers = [];
+
+    for(let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++){
+        pageNumbers.push(i);
+    }
+
+    return (
+        <nav>
+            <ul className="pagination">
+                {pageNumbers.map(num => (
+                    <li key={num} className="page-item">
+                        <a href="!#" className="page-link" onClick={() => paginate(num)}>{num}</a> 
+                    </li>
+                ))}    
+            </ul>             
+        </nav>
+    )
+}
+
+export default Pagination
